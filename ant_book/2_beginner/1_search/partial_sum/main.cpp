aa0c5aad2060815666e0c20e33f1d9cae7461c17
Nemuminia <nemuminia@gmail.com>
2021-10-14 22:57:37 +0900

    add comment to partial_sum

--- ant_book/2_beginner/1_search/partial_sum/main.cpp
+++ ant_book/2_beginner/1_search/partial_sum/main.cpp
@@ -11,6 +11,9 @@ bool dfs(int i, int sum) {
     if (i == n) {
         return sum == k;
     }
+    
+    // 集合の中からいくつか選び～的な問題は、各要素ごとに"選ぶ"or"選ばない"の2パターンがある
+    // 選ぶパターン, 選ばないパターンを再帰関数で全探索する
     return (dfs(i + 1, sum) || dfs(i + 1, sum + a[i]));
 }
 
