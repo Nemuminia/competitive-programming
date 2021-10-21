e87d4d66baa78d9147aba6742f4eef2ec4622ae2
Nemuminia <nemuminia@gmail.com>
2021-10-21 22:00:58 +0900

    sarumans_army

--- ant_book/2_beginner/2_greedy_algorithm/sarumans_army/main.cpp
+++ ant_book/2_beginner/2_greedy_algorithm/sarumans_army/main.cpp
@@ -3,6 +3,28 @@
 
 using namespace std;
 
+#define MAX_N 1000
+
 int main(){
+    int n, r;
+    int x[MAX_N];
+    cin >> n >> r;
+    for (int i = 0; i < n; i++) {
+        cin >> x[i];
+    }
+
+    sort(x, x + n);
+
+    int ans = 0;
+    int uncovered = 0;
+    for (int i = 0; i < n; i++) {
+        if (x[uncovered] + r < x[i]) {
+            ans++;
+            uncovered = i;
+        }
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
